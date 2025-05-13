c27b23d1fb5abea3a6389bf411cdef0ace6277fe
Tushar Ahuja <tusharahuja668@gmail.com>
2025-05-13 12:15:58 +0530

    change in repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  // Handle option selection
+  const handleSelectedOption = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ background: 'Green' }} id="main">
+      <h1 >Parent Component</h1>
+
+      {/* Pass the handleSelectedOption function to child components */}
+      <div style={{display:'flex',flexDirection:'column', gap:'0px'}}>
+      <ChildComponent1 onSelectOption={handleSelectedOption} />
+      <ChildComponent2 onSelectOption={handleSelectedOption} />
+      </div>
+      <span>Selected Option: {selectedOption}</span>
     </div>
-  )
+  );
+};
+
+function ChildComponent1({ onSelectOption }) {
+  return (
+    <div style={{background:'brown'}}>
+      <h1>Child Component 1</h1>
+      <button onClick={() => onSelectOption('Option 1')}>Option 1</button>
+    </div>
+  );
 }
 
-export default App
+function ChildComponent2({ onSelectOption }) {
+  return (
+    <div div style={{background:'yellow'}}>
+      <h1>Child Component 2</h1>
+      <button onClick={() => onSelectOption('Option 2')}>Option 2</button>
+    </div>
+  );
+}
+
+export default App;
